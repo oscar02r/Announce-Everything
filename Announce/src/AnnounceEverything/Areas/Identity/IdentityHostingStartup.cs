b4b1de15b2ec4270c77f8470b403aff07d1332bb
Oscary <oscary.ramirez@gmail.com>
2019-06-27 23:24:24 -0400

    Primera migracion de identity

---
+++ Announce/src/AnnounceEverything/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AnnounceEverything.Areas.Identity.Data;
+using AnnounceEverything.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AnnounceEverything.Areas.Identity.IdentityHostingStartup))]
+namespace AnnounceEverything.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DataContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DataContextConnection")));
+
+                services.AddDefaultIdentity<AppUser>()
+                    .AddEntityFrameworkStores<DataContext>();
+            });
+        }
+    }
+}
--- Announce/src/AnnounceEverything/Startup.cs
+++ Announce/src/AnnounceEverything/Startup.cs
@@ -51,6 +51,7 @@ namespace AnnounceEverything
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
