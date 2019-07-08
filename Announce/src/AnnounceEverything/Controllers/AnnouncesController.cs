d07240c8d97b076da621d7c3eb06297e64bb15f4
Oscary <oscary.ramirez@gmail.com>
2019-07-07 23:18:45 -0400

    Agregado de rutas, formulario crear y AnnounceViewModels

---
+++ Announce/src/AnnounceEverything/Controllers/AnnouncesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnnounceEverything.Controllers
+{
+    public class AnnouncesController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
