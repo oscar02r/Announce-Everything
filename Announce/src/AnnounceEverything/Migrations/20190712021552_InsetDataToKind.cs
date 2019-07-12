5d2de96aa529995c0032fdcb41217f08bb23a0d6
Oscary <oscary.ramirez@gmail.com>
2019-07-11 22:24:44 -0400

    Insercion de datos de Userkinds

---
+++ Announce/src/AnnounceEverything/Migrations/20190712021552_InsetDataToKind.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnnounceEverything.Migrations
+{
+    public partial class InsetDataToKind : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+               table: "UserKinds",
+               columns: new[] { "Id", "Name" },
+               values: new object[] { 1, "Paticular" }
+               );
+
+            migrationBuilder.InsertData(
+                table: "UserKinds",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Tienda" }
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+              table: "UserKinds",
+              keyColumn: "Id",
+              keyValue: 1
+              );
+
+            migrationBuilder.DeleteData(
+               table: "UserKinds",
+               keyColumn: "Id",
+               keyValue: 2
+               );
+        }
+    }
+}
