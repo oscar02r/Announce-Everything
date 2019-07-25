c25025a83aab22746628f909646d7a23b8ec684a
Oscary Ramirez <43319715+oscar02r@users.noreply.github.com>
2019-07-25 14:31:10 -0400

    Agregado de UserViewModel

---
+++ Announce/src/AnnounceEverything/ViewModels/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnnounceEverything.ViewModels
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public IEnumerable <SelectListItem> UserKind { get; set; }
+    }
+}
