d98a34fee17092e42cc0b3e846c794d317087589
Oscary <oscary.ramirez@gmail.com>
2019-07-25 23:15:55 -0400

    Insert datato TypeUser

---
+++ Announce/src/AnnounceEverything/Migrations/20190726031143_InsertDataIntoTypeUser.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnnounceEverything.Migrations
+{
+    public partial class InsertDataIntoTypeUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+              table: "TypeUsers",
+              columns: new[] { "Id", "Name" },
+              values: new object[] { 1, "Paticular" }
+              );
+
+            migrationBuilder.InsertData(
+                table: "TypeUsers",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Tienda" }
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+             table: "TypeUsers",
+             keyColumn: "Id",
+             keyValue: 1
+             );
+
+            migrationBuilder.DeleteData(
+               table: "TypeUsers",
+               keyColumn: "Id",
+               keyValue: 2
+               );
+        }
+    }
+}
