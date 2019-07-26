8ca29f814282afc6746225558d87bcf820294e81
Oscary <oscary.ramirez@gmail.com>
2019-07-25 22:02:53 -0400

    Agregando propiedades a AppAser

--- Announce/src/AnnounceEverything/Data/AppUser.cs
+++ Announce/src/AnnounceEverything/Data/AppUser.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using AnnounceEverything.Models;
 using Microsoft.AspNetCore.Identity;
 
 namespace AnnounceEverything.Data
 {
     public class AppUser : IdentityUser
     {
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string LastName { get; set; }
+       
+        public TypeUser TypeUser { get; set; }
     }
 }
---
+++ Announce/src/AnnounceEverything/Migrations/20190726020000_AddFieldToUser.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnnounceEverything.Migrations
+{
+    public partial class AddFieldToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TypeUserId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_TypeUserId",
+                table: "AspNetUsers",
+                column: "TypeUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_TypeUsers_TypeUserId",
+                table: "AspNetUsers",
+                column: "TypeUserId",
+                principalTable: "TypeUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_TypeUsers_TypeUserId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_TypeUserId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TypeUserId",
+                table: "AspNetUsers");
+        }
+    }
+}
